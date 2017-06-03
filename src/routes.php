2a6f60b604b96b5be02291e5197002bda2cd2ba4
muyege <muyege@gmail.com>
2017-06-03 11:52:40 +0000

    test

--- src/routes.php
+++ src/routes.php
@@ -8,3 +8,5 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+$app->get('/test', '\HomeController:show');
